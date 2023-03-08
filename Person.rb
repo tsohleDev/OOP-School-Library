da3a28fa10b9f61edf4ec3e1d7c87408a25a2620
tsohleDev <mokhemisitsohle@gmail.com>
2023-03-09 00:05:33 +0200

    Fix Linters

--- Person.rb
+++ Person.rb
@@ -1,32 +1,24 @@
-#globals
-$global_var = 0
-
 class Person
-    def initialize(age, name = "Unknown", parent_permission = nil)
-        @id = $global_var
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Math.random(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-        $global_var += 1
-    end
+  # getters for @id, @name, and @age
+  attr_reader :id
+  attr_accessor :name, :age
 
-    #getters for @id, @name, and @age
-    attr_reader :id
-    attr_reader :name
-    attr_reader :age
+  # setters for @name and @age
 
-    #setters for @name and @age
-    attr_writer :name
-    attr_writer :age
+  def of_age?
+    @age >= 18
+  end
+  # make is_of_age? private
+  private :is_of_age?
 
-    def is_of_age?
-        @age >= 18 ? true : false
-    end
-    #make is_of_age? private
-    private :is_of_age?
-
-    def can_use_services?
-        @parent_permission && is_of_age?() ? true : false
-    end
+  def can_use_services?
+    @parent_permission && is_of_age?
+  end
 end
--- Student.rb
+++ Student.rb
@@ -1,12 +1,12 @@
-require_relative 'Person'
+require_relative 'person'
 
 class Student < Person
-    def initialize(age, name, classroom, parent_permission = true)
-        super(age, name, parent_permission)
-        @classroom = classroom
-    end
-        
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
+  def initialize(age, name, classroom, parent_permission: true)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯(ツ)/¯'
+  end
 end

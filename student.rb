d69c1cc06db926d348a5392419dab1c6d855b19d
tsohleDev <mokhemisitsohle@gmail.com>
2023-03-09 10:12:21 +0200

    Create a one to many relationship between Classroom and Student

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require_relative 'person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, name, classroom, parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
@@ -8,5 +10,10 @@ class Student < Person
 
   def play_hooky
     '¯(ツ)/¯'
+  end
+
+  def add_to_classroom(classroom)
+    @classroom = classroom
+    classroom.students << self
   end
 end

741b5b74acf93780da98f7d094c1c97c5c636548
tsohleDev <mokhemisitsohle@gmail.com>
2023-03-09 16:11:26 +0200

    Make main executable

--- main.rb
+++ main.rb
@@ -1,9 +1,8 @@
-#!/usr/bin/env ruby
 require_relative 'person'
 require_relative 'student'
 require_relative 'teacher'
-require_relative 'book'
 require_relative 'rental'
+require_relative 'book'
 
 class App
   def initialize()
